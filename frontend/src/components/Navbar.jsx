6a95bfceab299325a99d226dd1725bc172d6996e
OJASVISWAMI222 <ojasviswami22@gmail.com>
2024-08-26 22:53:24 +0530

    frontend and navbar setup

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+const Navbar = () => {
+  return (
+      <nav className="navbar navbar-expand-lg bg-body-tertiary navnav">
+        <div className="container-fluid" >
+          <a className="navbar-brand" href="#">
+          TravelNest
+          </a>
+         
+          <div className="collapse navbar-collapse" >
+            <div className="navbar-nav">
+              <a className="nav-link abc mrgn" href="#">
+                Home
+              </a>
+              <a className="nav-link abc" href="/login">
+                Login
+              </a>
+              <a className="nav-link abc " href="/signup">
+                SignUp
+              </a>
+            </div>
+          </div>
+        </div>
+      </nav>
+  );
+};
+
+export default Navbar;
