f902c7a6512f6e3a00ad44391a3085b97740439f
OJASVISWAMI222 <ojasviswami22@gmail.com>
2024-12-28 21:27:08 +0530

    vercel routing issue

--- server/index.js
+++ server/index.js
@@ -14,7 +14,8 @@ const dbconfig = require("./db");
 const app = express();
 
 const corsOptions = {
-  origin: "*",
+  origin: ["https://travelnest-sand.vercel.app/","http://localhost:5173/"],
+  credentials:true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
